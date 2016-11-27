4b2fb1ce66c8089b99d3c4bba68711573d2d82a6
Chad Skeeters <chad.skeeters@gmail.com>
2016-11-27 12:40:04 -0600

    Added README.md and changed name of the program to halftone2src

---
+++ README.md
@@ -0,0 +1,21 @@
+Halftone 2 Source
+=================
+
+For creating c++ source code data images for 128x128 Sharp Memory LCDs.
+
+It uses [lodepng](http://lodev.org/lodepng/) from Lode Vandevenne.
+
+The output is 128 rows of 16 bytes where each bit represents a pixel being on or off.
+
+Usage
+-----
+
+First convert an existing 128x128 RGB png file and outputs a 128x128 half-tone image
+
+    brew install imagemagick
+    convert chad.png -brightness-contrast 30 -colorspace Gray -ordered-dither h4x4a chad_h4x4a.png
+
+Then use halftone2src to convert the RGB halftone PNG into source code (hex).
+
+    g++ lodepng.cpp halftone2src.cpp -ansi -pedantic -Wall -Wextra -O3 -o halftone2src
+    ./halftone2src chad_h4x4a.png
--- halftone2src.cpp
+++ halftone2src.cpp
@@ -1,21 +1,9 @@
-// For creating c++ source code data images for 128x128 Sharp LCD epaper displays
-// It uses lodepng from http://lodev.org/lodepng/ (Lode Vandevenne)
-// The output is 128 rows of 16 bytes where each bit represents a pixel being on or off.
-//
-// This takes a 128x128 RGB png file and outputs a 128x128 half-tone image
-//   brew install imagemagick
-//   convert chad.png -brightness-contrast 30 -colorspace Gray -ordered-dither h4x4a chad_h4x4a.png
-//
-// This takes the 128x128 half-tone image and outputs c++ hex codes
-//   g++ lodepng.cpp toHex.cpp -ansi -pedantic -Wall -Wextra -O3 -o toHex
-//   ./toHex chad_h4x4a.png
-
 #include "lodepng.h"
 #include <iostream>
 #include <iomanip>
 
 
-void toHex(const char* filename)
+void halftone2src(const char* filename)
 {
   std::vector<unsigned char> image; //the raw pixels
   unsigned width, height;
@@ -57,6 +45,6 @@ int main(int argc, char *argv[])
 {
   const char* filename = argc > 1 ? argv[1] : "chad_h4x4a.png";
 
-  toHex(filename);
+  halftone2src(filename);
 }
 
